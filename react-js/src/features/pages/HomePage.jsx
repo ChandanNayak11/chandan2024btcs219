3e3fbe9c0fb25af8ea44e09033a27f379c378ff9
Chandan Kumar Nayak <chandan1nayak9998@gmail.com>
2026-07-27 16:55:54 +0530

    Done

--- react-js/src/features/pages/HomePage.jsx
+++ react-js/src/features/pages/HomePage.jsx
@@ -13,14 +13,18 @@ const HomePage = () => {
     <div className="home-container">
         <div className="container">
             <h1>Welcome!</h1>
+
+            <div className="hero">
             <div className="flex">
             <p> Hii, I am {user.name} </p>
             <p>{user.shortBio}</p>
+            <button className="button" onClick={ () => navigate("/About")}> Know More </button>
             </div>
-            <img src="" alt="img1" />
+            <img src="https://blog-pixomatic.s3.appcnt.com/image/22/01/26/61f166e1377d4/_orig/pixomatic_1572877223091.png" alt="img1" />
+            </div>
             
 
-            <button className="button" onClick={ () => navigate("/About")}> Know More </button>
+            
         </div>
     </div>
   )
